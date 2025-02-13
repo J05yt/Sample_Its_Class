ff04196cf4d6750af02a8909cefeab74a892519e
J05yt <giacomo.iacoponi@itssmartacademy.it>
2025-02-13 18:01:42 +0100

    Add files via upload

--- esercizi js DOM/es js DOM 3.js
+++ esercizi js DOM/es js DOM 3.js
@@ -1,4 +1,7 @@
 function addparagraph(){
-    document.createElement<"p">
-    p.write("This is a paragraph")
+    let p1=document.createElement("p")
+    let p2=document.createTextNode("This is a paragraph")
+    p1.appendChild(p2)
+    document.body.appendChild(p2)
+
 }
